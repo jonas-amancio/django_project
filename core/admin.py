469287e85f9a0e6128e119f1a03978989fec0e9f
Jonas Amancio <jonas.amancio.dev@gmail.com>
2023-12-14 08:41:19 -0300

    django framework basico

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Produto, Cliente
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'estoque')
+
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'email')
+
+
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Produto, ProdutoAdmin)
--- core/views.py
+++ core/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.template import loader
+from django.http import HttpResponse
+from .models import Produto
 
-# Create your views here.
+
+def index(request):
+    if str(request.user) != "AnonymousUser":
+        logado = True
+    else:
+        logado = False
+
+    produtos = Produto.objects.all()
+
+    context = {
+        'curso': 'Django Web',
+        'linguagem': 'Python',
+        'logado': logado,
+        'produtos': produtos
+    }
+    return render(request, 'index.html', context)
+
+
+def contato(request):
+    return render(request, 'contato.html')
+
+
+def produto(request, pk):
+    #prod = Produto.objects.get(id=pk)
+    prod = get_object_or_404(Produto,id=pk)
+    context = {
+        'produto': prod
+    }
+    return render(request, 'produto.html', context)
+
+
+def error404(request, exception):
+    template = loader.get_template('404.html')
+    return HttpResponse(content=template.render(), content_type='text/html; charset=utf8', status=404)
+
+def error500(request):
+    template = loader.get_template('500.html')
+    return HttpResponse(content=template.render(), content_type='text/html; charset=utf8', status=500)
